35399c34ec90d33ea3f72dca5f9db87f918bb73f
Fangfei FAN <z5444929@ad.unsw.edu.au>
2025-11-21 17:43:31 +1100

    Deploy frontend using github.io

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GH_PAGES_BASE ?? '/',
   plugins: [react()],
   server: {
     port: 3000,
     open: true
   }
 })
-
